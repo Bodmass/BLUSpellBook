187b0ca95c5b979a867722f29ceecb7f3c3b1b26
Aziz <ararwow@gmail.com>
2021-06-19 15:04:59 +0100

    feat: Add next and ts config

---
+++ next-seo.config.js
@@ -0,0 +1,35 @@
+const title = 'FFXIV Blue Mage Spellbook'
+const description = 'Final Fantasy XIV Shadowbringers Blue Mage Spellbook. Updated for Patch 5.5 (13/04/2021)'
+const url = 'https://azizarar.com/'
+const images = [
+  {
+    url: 'https://ffxiv.azizarar.com/images/sss-image-01.jpg',
+    width: 800,
+    height: 600,
+    alt: 'SSS Image TEMP',
+  },
+]
+const tags = [
+  'FFXIV, Final Fantasy, Final Fantasy XIV, XIV, Final Fantasy 14, 14, Shadowbringers, Blu, Blue Mage, Spell, Book, Spellbook',
+]
+
+module.exports = {
+  title,
+  description,
+  canonical: url,
+  openGraph: {
+    title,
+    description,
+    url,
+    images,
+    site_name: title,
+    type: 'website',
+    locale: 'en_GB',
+    tags,
+  },
+  twitter: {
+    handle: '@bodmassad',
+    site: '@bodmassad',
+    cardType: 'summary_large_image',
+  },
+}
