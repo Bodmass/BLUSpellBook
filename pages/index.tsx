b8a53201cc8a73d69f363beb9737a7b62a94c6f1
Aziz <ararwow@gmail.com>
2021-06-19 17:59:48 +0100

    feat: Add spellbook to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Spellbook from '../components/Spellbook'
 
 export default function Home() {
   return (
@@ -12,11 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div>
-        <p>
-          <b>Hello World</b>
-        </p>
-      </div>
+      <Spellbook />
       <Footer />
     </div>
   )
